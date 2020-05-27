e2b86339f1c6f6afe47d0743486cb8bf3eda28b0
Leonel Ubeda <leonelubeda@hotmail.com>
2020-05-27 01:24:31 -0600

    purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,20 @@
-const tailwindcss = require('tailwindcss');
-module.exports = {
-    plugins: [
-        tailwindcss('./tailwind.js'),
-        require('autoprefixer')
+//Que ES PURGECSS?
+//https://medium.com/@xijo/create-react-app-with-tailwind-via-postcss-plus-purgecss-5c36b4c33ba7
+
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: [
+      './src/**/*.jsx',
+      './src/**/*.js',
+      './public/index.html'
     ],
-};
+    css: ['./src/assets/**/*.css', './src/assets/**/*.scss'],
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  })
+  module.exports = {
+    plugins: [
+      require('tailwindcss'),
+      require('autoprefixer'),
+      ...process.env.NODE_ENV === 'production' ? [purgecss] : []
+    ],
+  };
