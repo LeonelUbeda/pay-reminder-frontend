7b57a66689d71208c3b72026ab3c91e4999809aa
Leonel Ubeda <leonelubeda@hotmail.com>
2021-02-20 02:47:12 -0600

    :art: prettier format test

--- src/localStorage/history.js
+++ src/localStorage/history.js
@@ -1,6 +1,6 @@
-import { Histories } from './databases'
-import {v4 as uuidv4} from 'uuid'
-import moment from 'moment'
+import { Histories } from "./databases";
+import { v4 as uuidv4 } from "uuid";
+import moment from "moment";
 /*
 
 {
@@ -11,46 +11,45 @@ import moment from 'moment'
     paidAmount?: Integer,
     payment: 23094820934823094823094
 }
-
-
 */
 
-export async function getAllStoredHistories(){
-    let historiesKeys = await Histories.keys()
-    let histories = []
-    for (let key of historiesKeys){
-        histories.push(await Histories.getItem(key))
+export async function getAllStoredHistories() {
+  let historiesKeys = await Histories.keys();
+  let histories = [];
+  for (let key of historiesKeys) {
+    histories.push(await Histories.getItem(key));
+  }
+  return histories;
+}
+
+export async function createLocalHistory(history) {
+  return new Promise((resolve, reject) => {
+    if ("month" in history && "year" in history && "payment" in history) {
+      let { month, year, payment } = history;
+      let id = uuidv4();
+      let createdAt = moment().format("YYYY-MM-DD");
+      Histories.setItem(id, {
+        ...history,
+        id,
+        month,
+        year,
+        payment,
+        createdAt,
+      }).then((element) => {
+        resolve(element);
+      });
     }
-    return histories
+  });
 }
 
-
-export async function createLocalHistory(history){
-    return new Promise((resolve, reject) => {
-        if('month' in history && 'year' in history && 'payment' in history){
-
-            let {month, year, payment} = history
-            let id = uuidv4()
-            let createdAt = moment().format('YYYY-MM-DD')
-            Histories.setItem(id, {
-                ...history, id, month, year, payment, createdAt,
-            }).then(element => {
-                resolve(element)
-            })
-        }
-    })
+export async function findAndDeleteHistory(key) {
+  return new Promise((resolve, reject) => {
+    Histories.removeItem(key)
+      .then(() => {
+        resolve();
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
 }
-
-
-export async function findAndDeleteHistory(key){
-    return new Promise((resolve, reject) => {
-        Histories.removeItem(key)
-        .then(() => {
-            resolve()
-        })
-        .catch((error) => {
-            reject(error)
-        })
-    })
-}
-
